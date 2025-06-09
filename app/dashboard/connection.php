570ca9fc32991f9d4d50beabd1b5e1a26185d480
bmmrxx <mitzbo@outlook.com>
2025-06-09 12:07:28 +0200

    Connection comment

--- app/dashboard/connection.php
+++ app/dashboard/connection.php
@@ -10,8 +10,8 @@ class Connection
             // Establish the PDO connection
             $this->dbh = new PDO(
                 'mysql:host=localhost;port=3306;dbname=secondhand_market',
-                'root',
-                '1477'
+                'root', // Change if needed
+                '1477'// Change if needed
             );
             // Set the PDO error mode to exception
             $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
